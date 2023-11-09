cb7974c1811f41a843250fd66b3746b1b9327146
Surendra1923 <sam863282@gmail.com>
2023-11-09 08:37:20 +0530

    potd 9-11-23

---
+++ src/gfgpotd/pridect_the_column.java
@@ -0,0 +1,55 @@
+/*
+ * Given a matrix(2D array) M of size N*N consisting of 0s and 1s only. The task is to find the column with maximum number of 0s. If more than one column exists, print the one which comes first. If the maximum number of 0s is 0 then return -1.
+
+Example:
+
+Input:
+N = 3
+M[][] = {{0, 0, 0},
+          {1, 0, 1},
+          {0, 1, 1}}
+Output:
+0
+Explanation:
+0th column (0-based indexing) is having 2 zeros which is maximum among all columns and comes first.
+Your Task:
+Your task is to complete the function columnWithMaxZero() which should return the column number with the maximum number of zeros. 
+
+Expected Time Complexity: O(N * N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 103
+0 <= A[i][j] <= 1
+
+
+ */
+
+package gfgpotd;
+
+public class pridect_the_column {
+    
+
+    int columnWithMaxZeros(int arr[][], int N)
+    {
+        // code here 
+        int ans=-1;
+        
+        int max=0;
+        for(int i=0;i<N;i++){
+            int count=0;
+            for(int j=0;j<N;j++){
+                if(arr[j][i]==0){
+                    count++;
+                }
+            }
+            if(count>max){
+                max=count;
+                ans=i;
+            }
+            
+        }
+        return ans;
+    }
+
+}
